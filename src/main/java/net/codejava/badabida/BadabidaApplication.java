755ced3130c73fcf88d129618211be43cb99d270
MarcinDadura <56841909+MarcinDadura@users.noreply.github.com>
2021-01-02 16:48:49 +0100

    Init

---
+++ src/main/java/net/codejava/badabida/BadabidaApplication.java
@@ -0,0 +1,13 @@
+package net.codejava.badabida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadabidaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BadabidaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/codejava/badabida/BadabidaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.codejava.badabida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadabidaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
