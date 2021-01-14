e60748d9c06e9459a5060b5b434eb44cf33d8f54
MarcinDadura <56841909+MarcinDadura@users.noreply.github.com>
2021-01-14 01:57:29 +0100

    upload store.html for users without login in

---
+++ src/main/java/net/codejava/badabida/controllers/UnloggedController.java
@@ -0,0 +1,32 @@
+package net.codejava.badabida.controllers;
+
+import net.codejava.badabida.repos.CzescRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UnloggedController {
+
+    private final CzescRepository czescRepository;
+
+    public UnloggedController(CzescRepository czescRepository) {
+        this.czescRepository = czescRepository;
+    }
+
+    @GetMapping("/")
+    public String getAny() {
+        return "index";
+    }
+
+    @GetMapping("/start")
+    public String getIndex() {
+        return "index";
+    }
+
+    @GetMapping("/store")
+    public String getItemInfo(Model model) {
+        model.addAttribute("czesci", czescRepository.findAll());
+        return "store";
+    }
+}
