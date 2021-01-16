ab4acfb34d8ed752f30039f294b00ab07e7b6137
MarcinDadura <56841909+MarcinDadura@users.noreply.github.com>
2021-01-16 21:28:30 +0100

    change titles of all pages

--- src/main/java/net/codejava/badabida/security/SecurityConfig.java
+++ src/main/java/net/codejava/badabida/security/SecurityConfig.java
@@ -76,7 +76,6 @@ public class SecurityConfig {
                     .formLogin()
                     .loginPage("/employee/login")
                     .defaultSuccessUrl("/employee/home")
-                    .permitAll()
                     .and()
                     .logout()
                     .logoutUrl("/employee/logout")
