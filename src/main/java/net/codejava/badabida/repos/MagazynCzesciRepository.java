5e80cbb7c0a1e77d141937147ef00eebb1f4055e
MarcinDadura <56841909+MarcinDadura@users.noreply.github.com>
2021-01-19 23:32:26 +0100

    algin buttons in htms

--- src/main/java/net/codejava/badabida/repos/MagazynCzesciRepository.java
+++ src/main/java/net/codejava/badabida/repos/MagazynCzesciRepository.java
@@ -1,6 +1,5 @@
 package net.codejava.badabida.repos;
 
-import net.codejava.badabida.model.Czesc;
 import net.codejava.badabida.model.MagazynyCzesci;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,12 +7,4 @@ import java.util.List;
 
 public interface MagazynCzesciRepository extends JpaRepository<MagazynyCzesci, Long> {
     List<MagazynyCzesci> findAll();
-
-    MagazynyCzesci findByCzesc(Czesc czesc);
-
-    MagazynyCzesci findByCzesc_NrCzesci(Long nrCzesci);
-
-    void deleteByCzesc(Czesc czesc);
-
-    void deleteByCzesc_NrCzesci(Long nrCzesci);
 }
